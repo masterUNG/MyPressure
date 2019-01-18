ac0cd403948c6f9e98348b5e73ae541e7ae2270e
masterUNG <phrombutr@gmail.com>
2019-01-18 11:31:49 +0700

    Add Fragment to Activity Success

--- app/src/main/java/masterung/th/in/androidthai/mypressure/MainActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/mypressure/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+    }   // Main Method
+
+}   // Main Class
